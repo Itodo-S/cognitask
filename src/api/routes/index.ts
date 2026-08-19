360ef64f4e14dc80b30f9877949388a1a0ac3d01
Itodo-S <itodosimonitodo1@gmail.com>
2026-08-19 08:00:02 +0100

    feat(routes): add tag, SSE, and bulk route exports to barrel

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,3 +2,6 @@ export { todoRoutes } from "./todo.routes.js";
 export { aiRoutes } from "./ai.routes.js";
 export { sessionRoutes } from "./session.routes.js";
 export { statsRoutes } from "./stats.routes.js";
+export { tagRoutes } from "./tag.routes.js";
+export { sseRoutes } from "./sse.routes.js";
+export { bulkRoutes } from "./bulk.routes.js";
